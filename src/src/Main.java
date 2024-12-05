404faf2cca6b18a825b3f562992ea0364757b738
Dimo Yordanov <dimoy73@gmail.com>
2024-12-06 01:26:09 +0200

    Implemented file reading

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+//        read the two columns from the txt files
+//        sort the arrays, subtract the left from right element
+//        add the difference to a different sum
+//
+        BufferedReader reader;
+
+        try {
+            reader = new BufferedReader(new FileReader("input.txt"));
+            String line = reader.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                // read next line
+                line = reader.readLine();
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
