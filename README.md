decffbc4c18e31ec5bd7aeb8e4ad39991d435083
Dimo Yordanov <dimoy73@gmail.com>
2024-12-06 10:16:43 +0200

    day 1 part one complete

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Aoc2024
 AoC 2024 attempt
+
+**Problem set one notes:**
+# H1 - part one
+Had to read a file, split the two number columns into lists, sort the lists, 
+subtract corresponding index numbers. First attempt was a fail, because I didn't consider 
+that I should always check which element is bigger. Overall a fun task.
+
--- src/src/Main.java
+++ src/src/Main.java
@@ -1,30 +1,53 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-//        read the two columns from the txt files
-//        sort the arrays, subtract the left from right element
-//        add the difference to a different sum
-//
+
+        int[] tempArr;
+
+        List<Integer> leftList = new ArrayList<>();
+
+        List<Integer> rightList = new ArrayList<>();
+
         BufferedReader reader;
 
         try {
             reader = new BufferedReader(new FileReader("input.txt"));
             String line = reader.readLine();
-
             while (line != null) {
-                System.out.println(line);
-                // read next line
+                tempArr = Arrays.stream(line.split("\\s+")).mapToInt(Integer::parseInt).toArray();
+                leftList.add(tempArr[0]);
+                rightList.add(tempArr[1]);
                 line = reader.readLine();
             }
-
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        for (Integer i : leftList) {
+            System.out.println(i);
+        }
+
+        Collections.sort(leftList);
+        Collections.sort(rightList);
+
+        int sum = 0;
+
+        for (int i = 0; i < leftList.size(); i++) {
+            if (leftList.get(i) > rightList.get(i)) {
+                sum += leftList.get(i) - rightList.get(i);
+            } else {
+                sum += rightList.get(i) - leftList.get(i);
+            }
+        }
+
+        System.out.println(sum);
     }
 }
